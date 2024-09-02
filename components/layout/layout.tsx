275a0b1afb87f4b077af3190ac4aa9090b089a51
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-09-02 23:16:03 +0700

    feat: add Character carrd and Card comps

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,16 +1,30 @@
 import { NextPage } from 'next';
 import { PropsWithChildren } from 'react';
 import styles from '@/styles/Home.module.css';
-import { Navbar } from '../navbar/Navbar';
+import { Header } from '../header/Header';
+import styled from 'styled-components';
 
 export const Layout: NextPage<PropsWithChildren> = (props) => {
    const { children } = props;
    return (
-      <main className={`${styles.main}`}>
-         <Navbar />
-         {children}
-      </main>
+      <Container className={`${styles.main}`}>
+         <Header />
+         <Main>{children}</Main>
+      </Container>
    );
 };
 
 export const getLayout = (page: React.ReactElement) => <Layout>{page}</Layout>;
+
+const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   padding: 20px;
+`;
+
+const Main = styled.div`
+   width: 100%;
+   padding-bottom: 10px;
+   overflow: hidden;
+`;
