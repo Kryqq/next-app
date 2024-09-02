f5fb1186f7c65c90c8c59d78c60de6955a94be45
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-09-02 23:16:39 +0700

    feat: add tanstack query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 import { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { useLoader } from '@/assets/hooks/useLoader';
+import '../styles/nprogress.css';
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
    getLayout?: (page: ReactElement) => ReactNode;
@@ -12,6 +16,15 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
    const getLayout = Component.getLayout ?? ((page) => page);
+   const [queryClient] = React.useState(() => new QueryClient());
 
-   return getLayout(<Component {...pageProps} />);
+   useLoader();
+
+   return getLayout(
+      <QueryClientProvider client={queryClient}>
+         <HydrationBoundary state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+         </HydrationBoundary>
+      </QueryClientProvider>
+   );
 }
