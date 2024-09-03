47c4d5885e7650dc1d4f6fa5bf5c72f07b066240
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-09-03 23:06:36 +0700

    feat: add [id]routes

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,19 +1,75 @@
 import React from 'react';
-import { CharacterCard } from '@/components/card/characterCard/CharacterCard';
+import CharacterCard from '@/components/card/characterCard/CharacterCard';
 import { HeadMeta } from '@/components/headMeta/HeadMeta';
-import { getLayout } from '@/components/layout/layout';
-import { useCharacter } from '@/assets/hooks/useCharacter';
+import { BaseLayout } from '@/components/layout/baseLayout/BaseLayout';
+import { API } from '@/assets/api/api';
+import { CharacterType } from '@/assets/api/rick-and-morty-api';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import { PageWrapper } from '@/components/pageWrapper/PageWrapper';
 
-const Character = () => {
-   const character = useCharacter();
+export const getStaticPaths: GetStaticPaths = async () => {
+   const { results } = await API.rickAndMorty.getCharacters();
+
+   const paths = results.map((character) => ({
+      params: { id: character.id.toString() },
+   }));
+
+   return {
+      paths,
+      fallback: true,
+   };
+};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+   const { id } = params || {};
+
+   const character = await API.rickAndMorty.getCharacter(id as string);
+
+   if (!character) {
+      return {
+         notFound: true,
+      };
+   }
+
+   return {
+      props: {
+         character,
+      },
+   };
+};
+
+type Props = {
+   character: CharacterType;
+};
+
+const Character = (props: Props) => {
+   const { character } = props;
+
+   const router = useRouter();
+
+   if (router.isFallback) {
+      return <div>Loading...</div>;
+   }
+
+   const characterId = router.query.id;
+
+   const goToCharacters = () => {
+      router.push('/characters');
+   };
 
    return (
       <>
          <HeadMeta title={'Character'} />
-         {character && <CharacterCard key={character.id} character={character} />}
+         {character && (
+            <PageWrapper title="Character">
+               <h1>{characterId}</h1>
+               <CharacterCard key={character.id} character={character} />
+               <button onClick={goToCharacters}>GO TO CHARACTERS</button>
+            </PageWrapper>
+         )}
       </>
    );
-}
+};
 
-Character.getLayout = getLayout;
+Character.getLayout = (page: React.ReactElement) => <BaseLayout>{page}</BaseLayout>;
 export default Character;
