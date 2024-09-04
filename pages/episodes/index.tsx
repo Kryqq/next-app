a02476924f12246cc1cdb151ac6df35f4532a56e
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-09-04 22:46:48 +0700

    feat: add auth check

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -3,16 +3,29 @@ import React from 'react';
 import { API } from '@/assets/api/api';
 import { EpisodeType, ResponseType } from '@/assets/api/rick-and-morty-api';
 import { PageWrapper } from '@/components/pageWrapper/PageWrapper';
-import { Header } from '@/components/header/Header';
+
 import { Card } from '@/components/card/Card';
 import { BaseLayout, getLayout } from '@/components/layout/baseLayout/BaseLayout';
+import { GetServerSideProps } from 'next';
 
-export const getServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+   res.setHeader('Cache-Control', 'public, s-maxage=10 , stale-while-revalidate = 100');
+
    const episodes = await API.rickAndMorty.getEpisodes();
 
+   const isAuth = true;
    if (!episodes) {
       return {
          notFound: true,
+      };
+   }
+
+   if (!isAuth) {
+      return {
+         redirect: {
+            destination: '/characters',
+            permanent: false,
+         },
       };
    }
 
@@ -39,3 +52,4 @@ Episodes.getLayout = (page: React.ReactElement) => {
    return <BaseLayout>{page}</BaseLayout>;
 };
 export default Episodes;
+
